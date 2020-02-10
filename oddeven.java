051305ab4108cbe483e3f64dd31b917a86e99e66
bonnaskr <60335158+bonnaskr@users.noreply.github.com>
2020-02-10 09:41:07 +0600

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class oddeven {
+
+
+		    public static void main(String[] args) {
+		        int num;
+			Scanner input=new Scanner(System.in);
+			System.out.print("Enter Any numer:");
+			num=input.nextInt();
+			if(num%2==0){
+			    System.out.println("The Number is Even."+" "+num);
+
+		        }
+			else{
+			    System.out.println("The number is odd."+" "+num);
+		    }
+		    }
+		}
+
+
